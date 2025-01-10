0d248b1764e22d3eb8e6205518b62f91a468c75a
Salome Karseladze <saly13071@gmail.com>
2025-01-10 18:52:29 +0400

    add admin container box

--- src/Pages/LoginPage/Login.tsx
+++ src/Pages/LoginPage/Login.tsx
@@ -1,7 +1,24 @@
+import "./Login.css";
+
 function Login() {
   return (
-    <div>
-      <p>this is login</p>
+    <div className="main-container">
+      <div className="card-container">
+        <h1>Admin Panel login</h1>
+        <div className="input-container">
+          <p>
+            <label>Email</label>
+            <input type="email" />
+          </p>
+          <p>
+            <label>Password </label>
+            <input type="password" />
+          </p>
+        </div>
+        <div className="LoginButton">
+          <button>Sign In</button>
+        </div>
+      </div>
     </div>
   );
 }
