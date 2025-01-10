023813d37711dc01b5957f4354a749c373cb3c61
AniJijiashvili <ani.jijiashvili.1997@gmail.com>
2025-01-11 01:54:07 +0400

    made user page

--- src/Pages/UserPage/Users.tsx
+++ src/Pages/UserPage/Users.tsx
@@ -1,9 +1,21 @@
-function UsersPage() {
-  return (
-    <div>
-      <p> this is user page</p>
-    </div>
-  );
-}
 
-export default UsersPage;
+import UserPage from "../../Components/UserTable/UserPage";
+
+
+// function UsersPage() {
+//   return (
+//     <div>
+//       <p> this is user page</p>
+//     </div>
+//   );
+// }
+
+// export default UsersPage;
+const Users = () =>{
+  return(
+    <div>
+      <UserPage/>
+    </div>
+  )
+}
+export default Users
