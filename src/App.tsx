bda3a6151b69d1cf7eced8a38327fe9f23e89254
Salome Karseladze <saly13071@gmail.com>
2025-01-10 17:07:10 +0400

    add pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        <p> Admin Panel </p>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import Users from "./Pages/UserPage/Users.tsx";
+import Login from "./Pages/LoginPage/Login.tsx";
 
-createRoot(document.getElementById('root')!).render(
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/user",
+        element: <Users />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
