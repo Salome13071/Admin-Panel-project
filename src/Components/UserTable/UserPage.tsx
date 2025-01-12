743a0a0ae3c7f5f14b5281b66b98b9d0c152e947
AniJijiashvili <ani.jijiashvili.1997@gmail.com>
2025-01-12 05:48:37 +0400

    change markup of users and added pagination

--- src/Components/UserTable/UserPage.tsx
+++ src/Components/UserTable/UserPage.tsx
@@ -7,46 +7,76 @@ interface User {
   firstName: string;
   email: string;
   age: number;
-  role: string;
+  status: string;
 }
 
 const UserPage = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [page, setPage] = useState(1);
+  const [limit] = useState(15);
+
   useEffect(() => {
-    fetch("https://dummyjson.com/users")
+    fetch(
+      `https://dummyjson.com/users?limit=${limit}&skip=${(page - 1) * limit}`
+    )
       .then((info) => info.json())
-      .then((data) => setUsers(data.users));
-  }, []);
+      .then((data) => {
+        const users = data.users.map((user: any) => ({
+          id: user.id,
+          firstName: `${user.firstName}`,
+          email: user.email,
+          age: user.age,
+          status: user.isVerified ? "Verified" : "Unverified",
+        }));
+        setUsers(users);
+      });
+  }, [page]);
+
   return (
-    <>
-      <Header />
-      <div className="table-container">
-        <table>
-          <thead>
-            <tr className="table-heading">
-              <th>Name</th>
-              <th>Email</th>
-              <th>Age</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody className="table-content">
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.firstName}</td>
-                <td>{user.email}</td>
-                <td className="table-content-age">{user.age}</td>
-                <td className="table-content-status">{user.role}</td>
-                <td className="table-content-btns">
-                  <button className="table-content-edit">Edit</button>
-                  <button className="table-content-delete">Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div className="user-container">
+      <div className="user-title">
+        <p>Actions</p>
+        <p>First Name</p>
+        <p>Email</p>
+        <p>Age</p>
+        <p>Status</p>
       </div>
-    </>
+      <div className="user-search"></div>
+      {users.map((user, index) => (
+        <div
+          className="user-details"
+          key={user.id}
+          style={{
+            backgroundColor: index % 2 === 0 ? "white" : "rgb(238, 234, 234)",
+          }}
+        >
+          <div className="user-action">
+            <button className="user-action--edit">Edit</button>
+            <button className="user-action--delete">Delete</button>
+          </div>
+          <p className="user-details--firstname">{user.firstName}</p>
+          <p className="user-details--email">{user.email}</p>
+          <p className="user-details--age">{user.age}</p>
+          <p className="user-details--status">{user.status}</p>
+        </div>
+      ))}
+      <div className="pagination-controls">
+        <button
+          className="previus"
+          onClick={() => setPage((prevPage) => Math.max(prevPage - 1, 1))}
+          disabled={page === 1}
+        >
+          Previous
+        </button>
+        <button
+          className="Next"
+          onClick={() => setPage((prevPage) => prevPage + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 };
+
 export default UserPage;
